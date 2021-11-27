66e3bf4fc9a2df4e0791fcbaae3eb24597f60d82
Danilo Pitombeira <danilo_pitombera@hotmail.com>
2021-11-27 16:43:07 -0300

    Ajustes de Link na Queue do envio de Mensagem

--- app/Jobs/TransactionMessage.php
+++ app/Jobs/TransactionMessage.php
@@ -33,7 +33,7 @@ class TransactionMessage implements ShouldQueue
     public function handle()
     {
 
-        $sendMenssage = Http::get('https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04');
+        $sendMenssage = Http::retry(5,300)->get(config('services.transaction.url_message'));
 
         if($sendMenssage->failed()){
             TransactionMessage::dispatch($this->transaction);
--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
 
     'transaction' => [
         'url_validation' => env('url_validation','https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6'),
-        'url_message' => env('url_message', 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04')
+        'url_message' => env('url_message', 'http://o4d9z.mocklab.io/notify')
     ]
 
 ];
